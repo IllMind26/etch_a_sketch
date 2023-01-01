13b2514d44f83712dd20ce34882e8e1ae790a5e9
IllMind26 <104080920+IllMind26@users.noreply.github.com>
2022-12-31 19:27:17 -0600

    updated files

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const grid = document.createElement('div');
-const resetBtn = document.querySelector('button');
+const resetBtn = document.querySelector('#1');
 const container = document.querySelector('.container');
 
