6ed43acc91a01e0163de542bbdd1057efbc4407a
IllMind26 <104080920+IllMind26@users.noreply.github.com>
2023-01-10 23:46:58 -0600

    updated app.js and index.html

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const grid = document.createElement('div');
-const container = document.querySelector('.container');
+const container = document.body.querySelector('.container');
 const resetBtn = document.querySelector('#reset');
 const newBtn = document.querySelector('#new');
 const colorBtn = document.querySelector('#color');
 
+const grid = document.createElement('div');
+grid.classList.add('grids');
+container.appendChild(grid);
+document.body.append(container);
