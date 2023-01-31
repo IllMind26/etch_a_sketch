788f324054eb30d1afdba05dd08c3672d1964888
IllMind26 <104080920+IllMind26@users.noreply.github.com>
2023-01-30 21:10:05 -0600

    updated current files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # etch_a_sketch
 
-This another beginner project to improve my knowledge of Javascript, more importantly manipulating the DOM.
+This another beginner project to improve my knowledge of Javascript, more importantly manipulating the DOM. In this project, a grid will be created through the DOM instead of using standard HTML. Also, an Event Listener along with some CSS will be used in order to create the etch-a-sketch effect.
--- app.js
+++ app.js
@@ -1,9 +1,32 @@
-const container = document.body.querySelector('.container');
+// const container = document.body.querySelector('.container');
 const resetBtn = document.querySelector('#reset');
 const newBtn = document.querySelector('#new');
 const colorBtn = document.querySelector('#color');
 
-const grid = document.createElement('div');
-grid.classList.add('grids');
-container.appendChild(grid);
-document.body.append(container);
+function makeGrid(content) {
+    let container = content.appendChild(document.createElement('div'));
+    container.id = 'main';
+    container.classList.add('container');
+
+    for (let i = 0; i < 16; i++) {
+        let row = container.appendChild(document.createElement('div'));
+        row.classList.add('row');
+        row.id = 'row' + i;
+        for (let j = 0; j < 16; j++) {
+            let box = row.appendChild(document.createElement('div'));
+            box.classList.add('box');
+
+        }
+    }
+
+}
+
+makeGrid(document.body)
+
+
+// const grid = document.createElement('div');
+// grid.classList.add('grid-div');
+// container.appendChild(grid);
+// document.body.append(container);
+
+
