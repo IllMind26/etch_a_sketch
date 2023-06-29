ded463a70454e3db5ce2bae037a4b10c5027aeeb
IllMind26 <104080920+IllMind26@users.noreply.github.com>
2023-06-28 20:52:50 -0500

    updated files

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
+const defaultColor = '#333333';
+const defaultSize = 16;
+const defaultMode = 'color';
+
+let currentColor = defaultColor;
+let currentSize = defaultSize;
+let currentMode = defaultMode;
+
+
 const container = document.body.querySelector('.container');
 const resetBtn = document.querySelector('#reset');
 const newBtn = document.querySelector('#new');
 const colorBtn = document.querySelector('#color');
+const sixteenBtn = document.getElementsByName('sixteen');
+const thirtyTwoBtn = document.getElementsByName('thirtytwo');
+const sixtyFourBtn = document.getElementsByName('sixtyfour');
 
-function makeGrid(size) {
-    let container = document.querySelector('.container');
-    container.id = 'main';
-    container.classList.add('container');
-    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${size}, 1fr);`
-
-    let numDivs = size * size;
-
-    for (let i = 0; i < numDivs; i++) {
-        let div = document.createElement('div')
-        div.classList.add('row');
-        container.insertAdjacentElement('beforeend', div);
-    }
-
+const setNewColor = (newColor) => {
+    currentColor = newColor;
 }
 
-makeGrid(16)
+const setNewMode = (newMode) => {
+    currentMode = newMode;
+}
+
+const setNewSize = (newSize) => {
+    currentSize = newSize;
+}
 
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 
